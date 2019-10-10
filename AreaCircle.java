d0cd79d15908bafffd39500288fe513217ffb7a5
jeffinboban <32698202+jeffinboban@users.noreply.github.com>
2019-10-10 12:04:32 +0530

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,12 @@
+class AreaCircle
+{
+public static void main(String []args)
+{
+double rad=254.6;
+double pi=22.0/7;
+
+int area=(int)(pi*((rad*rad)/(100*100)));
+
+System.out.println("Area of the circle with the given radius is "+area);
+}
+}
